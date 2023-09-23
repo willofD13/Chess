121806ee61990ce6808f921b876402456fee0a94
willofD13 <leonidastsiakas13@gmail.com>
2023-09-23 18:11:48 +0300

    Initialize game variables

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,2 +1,12 @@
 class Chess
+
+    def initialize 
+        @chess_board = Array.new(8) { Array.new(8,'_')}
+        @white_pieces = 16 
+        @black_pieces = 16 
+        @turn = 1
+        @player_1 = nil
+        @player_2 = nil 
+    end
+    
 end
