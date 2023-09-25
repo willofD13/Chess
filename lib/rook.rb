77e49a66f227f7a3e3f66b2ab8eb99f2fec07729
willofD13 <leonidastsiakas13@gmail.com>
2023-09-25 17:11:09 +0300

    Create rook class

---
+++ lib/rook.rb
@@ -0,0 +1,12 @@
+class Pawn < Piece
+
+    def initialize(current_square,color)
+        @color = color
+        @current_square = current_square
+        @moves = [[1,0],[0,1],[-1,0],[0,-1]]
+    end
+
+    def to_s 
+        @color == 'black' ? '♖' : '♜'
+    end
+end
