39214ed6869a1f93bc355c27835d96998692e557
willofD13 <leonidastsiakas13@gmail.com>
2023-09-25 15:17:44 +0300

    Initialize black symbol

--- lib/Pawn.rb
+++ lib/Pawn.rb
@@ -2,7 +2,7 @@ class Pawn
 
     def initialize
         @symbol = '♟︎' if Chess.turn.odd?
-        @symbol = '' if Chess.turn.even?
+        @symbol = '♙' if Chess.turn.even?
         @type = 'pawn'
         @current_square = nil
     end
