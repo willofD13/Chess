f3fd0c028d5207fd77598744138e149bdd5faf74
willofD13 <leonidastsiakas13@gmail.com>
2023-09-25 17:13:34 +0300

    Create knight class

---
+++ lib/knight.rb
@@ -0,0 +1,12 @@
+class Knight < Piece
+
+    def initialize(current_square,color)
+        @color = color
+        @current_square = current_square
+        @moves = [[+2,+1], [+2,-1], [+1,+2], [+1,-2], [-1,+2], [-1,-2], [-2,+1], [-2,-1]]
+    end
+
+    def to_s 
+        @color == 'black' ? '♘' : '♞'
+    end
+end
