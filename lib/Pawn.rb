3827e4db2bb425413d98af9a409f731089674887
willofD13 <leonidastsiakas13@gmail.com>
2023-09-25 17:25:07 +0300

    Add capture moves to the pawn

--- lib/Pawn.rb
+++ lib/Pawn.rb
@@ -2,9 +2,8 @@ class Pawn < Piece
 
     def initialize(current_square,color)
         @color = color
-        @type = 'pawn'
         @current_square = current_square
-        @moves = [[1,0],[2,0]]
+        @moves = [[1,0],[2,0],[1,-1],[1,1]]
     end
 
     def to_s 
