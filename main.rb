96648ae36b2d5d2ae1bc3ff7e09ae7dd36c0b434
willofD13 <leonidastsiakas13@gmail.com>
2023-09-26 20:41:03 +0300

    Try enemy check method

--- main.rb
+++ main.rb
@@ -6,3 +6,9 @@ require_relative './lib/king.rb'
 require_relative './lib/queen.rb'
 require_relative './lib/piece.rb'
 require_relative './lib/board.rb'
+
+b = Board.new 
+b[[0,0]] = Pawn.new('black',b)
+b[[0,1]] = Pawn.new('white',b)
+piece = b[[0,0]]
+puts piece.is_there_an_enemy?([0,1])
