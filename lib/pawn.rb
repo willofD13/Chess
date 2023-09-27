e7e003faaa7c05a6efcc42b02011300b2ee54291
willofD13 <leonidastsiakas13@gmail.com>
2023-09-27 20:21:01 +0300

    Calculate pawn moves in each occasion

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,7 +1,7 @@
 require_relative 'piece.rb'
 require_relative 'stepable.rb'
 class Pawn < Piece
-    include Stepable
+    #include Stepable
     def move_dirs
         @color == 'black' ? [[1,0],[2,0],[1,-1],[1,1]] : [[-1,0],[-2,0],[-1,1],[-1,-1]]
     end
@@ -13,11 +13,19 @@ class Pawn < Piece
     def valid_moves(location,board)
         moves = []
         
-        move_dirs.each do |(dr,dc)|
+        move_dirs.each do |dir|
             current_r, current_c = location
-
-            current_r += dr 
-            current_c += dc
+            new_r = current_r + dir[0] 
+            new_c = current_c + dir[1]
+            if dir == move_dirs[2] || dir == move_dirs[3]
+                moves << [new_r,new_c] if is_there_an_enemy?([new_r,new_c])
+            elsif 
+                dir == move_dirs[1]
+                moves << [new_r,new_c] if (current_r == 1 || current_r == 6) && board.empty?([new_r,new_c])  
+            elsif 
+                dir == move_dirs[0]
+                moves << [new_r,new_c] if board.in_bounds?([new_r,new_c]) && board.empty?([new_r,new_c])
+            end
 
         end
         moves
