625877cbb5e0855566b2756be443916629484862
willofD13 <leonidastsiakas13@gmail.com>
2023-09-27 19:44:13 +0300

    Change directions based on pawn color

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -3,11 +3,24 @@ require_relative 'stepable.rb'
 class Pawn < Piece
     include Stepable
     def move_dirs
-        [[1,0],[2,0],[1,-1],[1,1]]
+        @color == 'black' ? [[1,0],[2,0],[1,-1],[1,1]] : [[-1,0],[-2,0],[-1,1],[-1,-1]]
     end
 
     def to_s 
         @color == 'black' ? '♙' : '♟︎'
     end
+
+    def valid_moves(location,board)
+        moves = []
+        
+        move_dirs.each do |(dr,dc)|
+            current_r, current_c = location
+
+            current_r += dr 
+            current_c += dc
+
+        end
+        moves
+    end  
 end
 
