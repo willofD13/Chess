de660f1a0984df481a9d16e7f725e025d1387352
willofD13 <leonidastsiakas13@gmail.com>
2023-09-27 12:27:19 +0300

    Fix loading order

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,8 @@
+require_relative './lib/piece.rb'
 require_relative './pawn.rb'
 require_relative './rook.rb'
-require_relative './bishop.rb'
 require_relative './knight.rb'
+require_relative './bishop.rb'
 require_relative './king.rb'
 require_relative './queen.rb'
 
