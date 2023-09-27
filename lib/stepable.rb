01f6adda29ec918ccf5e6711b391acae8a571491
willofD13 <leonidastsiakas13@gmail.com>
2023-09-27 19:23:47 +0300

    Check if the target square is in bounds

--- lib/stepable.rb
+++ lib/stepable.rb
@@ -6,7 +6,7 @@ module Stepable
             current_r, current_c = location
             current_r += dr 
             current_c += dc
-            moves << [current_r,current_c] if board.empty?([current_r,current_c]) || is_there_an_enemy?([current_r,current_c])
+            moves << [current_r,current_c] if board.in_bounds?([current_r,current_c]) && (board.empty?([current_r,current_c]) || is_there_an_enemy?([current_r,current_c]))
         end
         moves
     end  
