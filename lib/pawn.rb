c16f1790308f26d7ba827ef2d9cd5294185c122a
willofD13 <leonidastsiakas13@gmail.com>
2023-09-28 14:29:28 +0300

    Remove stepable logic from pawn

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,7 +1,6 @@
 require_relative 'piece.rb'
-require_relative 'stepable.rb'
 class Pawn < Piece
-    #include Stepable
+   
     def move_dirs
         @color == 'black' ? [[1,0],[2,0],[1,-1],[1,1]] : [[-1,0],[-2,0],[-1,1],[-1,-1]]
     end
