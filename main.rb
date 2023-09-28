b020eee988e93a05ea6c20718887875942f8a58a
willofD13 <leonidastsiakas13@gmail.com>
2023-09-28 18:28:02 +0300

    Check pawn movement

--- main.rb
+++ main.rb
@@ -8,10 +8,10 @@ require_relative './lib/queen.rb'
 require_relative './lib/board.rb'
 
 b = Board.new 
-b.add_pawns
-b.add_rest_pieces
-b[[0,2]] = Knight.new('black',b,[0,2])
-b[[0,2]].move_piece(b,[0,2],[2,3])
-b[[6,4]] = Pawn.new('white',b,[6,4])
-b[[6,4]].move_piece(b,[6,4],[4,4])
+b[[1,3]] = Pawn.new('black',b,[1,3])
+b[[1,3]].move_piece(b,[1,3],[3,3])
+b[[3,3]].move_piece(b,[3,3],[4,3])
+b[[4,3]].move_piece(b,[4,3],[5,3])
+b[[5,3]].move_piece(b,[5,3],[6,3])
+b[[6,3]].move_piece(b,[6,3],[7,3])
 b.display_board
