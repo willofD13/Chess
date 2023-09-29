bc2ebd4da4b7096221de598af93091decdc74fb9
willofD13 <leonidastsiakas13@gmail.com>
2023-09-29 14:23:33 +0300

    Load pieces in one file

---
+++ lib/pieces.rb
@@ -0,0 +1,7 @@
+require_relative './lib/piece.rb'
+require_relative './lib/pawn.rb'
+require_relative './lib/rook.rb'
+require_relative './lib/knight.rb'
+require_relative './lib/bishop.rb'
+require_relative './lib/king.rb'
+require_relative './lib/queen.rb'
