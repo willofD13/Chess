a02f856793cf19418cc8424d9c0dd84c96db2d2e
willofD13 <leonidastsiakas13@gmail.com>
2023-09-30 18:45:05 +0300

    Obtain checking piece direction to the king

--- lib/escapable.rb
+++ lib/escapable.rb
@@ -46,4 +46,23 @@ module Escapable
         checking_piece
     end
 
+    def king_dir(color)
+        king_loc  = pieces.find { |e| e.color == color && e.class == King}.location
+        checking_piece = find_checking_piece(color)[0]
+
+        checking_piece.move_dirs.each do |(dr,dc)|
+            c_r,c_c = find_checking_piece(color)[0].location
+            loop do
+                c_r += dr
+                c_c += dc
+                if c_r == king_loc[0] && c_c == king_loc[1]
+                    return [dr,dc]
+                elsif 
+                   !self.in_bounds?([c_r,c_c])
+                   break
+                end
+            end
+        end
+    end
+
 end
