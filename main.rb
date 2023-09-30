9b9487f25facdeaf6ab32be126ded74c6a021828
willofD13 <leonidastsiakas13@gmail.com>
2023-09-30 18:45:43 +0300

    Test king_dir method

--- main.rb
+++ main.rb
@@ -2,16 +2,8 @@ require_relative './lib/pieces.rb'
 require_relative './lib/board.rb'
 
 b = Board.new 
-b[[0,4]] = Rook.new('black',b,[0,4])
-b[[0,3]] = Queen.new('black',b,[0,3])
-b[[2,4]] = King.new('white',b,[2,4])
-b[[0,5]] = Queen.new('black',b,[0,5])
-#p b.check?('white')
-#p b.go_away?('white')
-b[[4,3]] = Queen.new('black',b,[4,3])
-b[[4,5]] = Queen.new('black',b,[4,5])
-b[[2,0]] = Rook.new('black',b,[2,0])
-
-#p b.go_away?('white')
+b[[1,3]] = King.new('black',b,[1,3])
+b[[3,4]] = Knight.new('white',b,[3,4])
+#b[[3,3]] = Bishop.new('white',b,[3,3])
 b.display_board
-p b.check?('white')
+p b.king_dir('black')
