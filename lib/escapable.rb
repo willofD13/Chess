0af73aba8d653af1db3766fcdf159e18caf797e8
willofD13 <leonidastsiakas13@gmail.com>
2023-09-30 13:37:34 +0300

    Check if a king can get away from danger

---
+++ lib/escapable.rb
@@ -0,0 +1,22 @@
+module Escapable
+    def go_away?(color)
+        king  = pieces.find { |e| e.color == color && e.class == King}
+
+        king_moves = king.valid_moves(king.location,self)
+        forbidden_moves = []
+        king_moves.each do |k_m|
+            pieces.select { |p| p.color != color }.each do |piece|
+                if piece.valid_moves(piece.location,self).include?(k_m)
+                    forbidden_moves << k_m
+                    break
+                end
+            end
+        end
+        
+        if forbidden_moves == king_moves
+            return false
+        else
+            return true
+        end
+    end
+end
